cfb756a5059d52be097bb7d3090d067797fe9465
rafaelsantos1000 <rafaasfs@gmail.com>
2021-06-14 14:32:37 +0100

    Repository

--- SuperShop/Data/SeedDb.cs
+++ SuperShop/Data/SeedDb.cs
@@ -1,8 +1,8 @@
-﻿using SuperShop.Data.Entities;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Linq;
 using System.Threading.Tasks;
+using SuperShop.Data.Entities;
+
 
 namespace SuperShop.Data
 {
--- SuperShop/Startup.cs
+++ SuperShop/Startup.cs
@@ -26,7 +26,8 @@ namespace SuperShop
             });
 
             services.AddTransient<SeedDb>();
-
+            services.AddScoped<IRepository, Repository>();
+           
   
             services.AddControllersWithViews();
         }
