ce06ce51ebe02c8c43d0cbe5dcc002b15a6e0c2b
rafaelsantos1000 <rafaasfs@gmail.com>
2021-07-08 11:47:55 +0100

    Cascade Delete Rule

--- SuperShop/Controllers/ProductsController.cs
+++ SuperShop/Controllers/ProductsController.cs
@@ -56,7 +56,6 @@ namespace SuperShop.Controllers
 
 
         // GET: Products/Create
-        [Authorize(Roles ="Admin")]
         public IActionResult Create()
         {
             return View();
@@ -177,11 +176,31 @@ namespace SuperShop.Controllers
         // POST: Products/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var product = await _productRepository.GetByIdAsync(id);
-            await _productRepository.DeleteAsync(product);
-            return RedirectToAction(nameof(Index));
+
+            try
+            {
+                //throw new Exception("Excepção de Teste");
+                await _productRepository.DeleteAsync(product);
+                return RedirectToAction(nameof(Index));
+            }
+            catch (DbUpdateException ex)
+            {
+
+                if (ex.InnerException != null && ex.InnerException.Message.Contains("DELETE"))
+                {
+                    ViewBag.ErrorTitle = $"{product.Name} provavelmente está a ser usado!!";
+                    ViewBag.ErrorMessage = $"{product.Name} não pode ser apagado visto haverem encomendas que o usam.</br></br>" +
+                        $"Experimente primeiro apagar todas as encomendas que o estão a usar," +
+                        $"e torne novamente a apagá-lo"; 
+                }
+
+                return View("Error");
+            }
+
         }
 
         public IActionResult ProductNotFound()
